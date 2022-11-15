f6e028b22369a14f3f3bb283ba61008de6604c99
elvirafirmansyah <freddyfirmansyah25@gmail.com>
2022-11-15 22:00:28 +0700

    feat: card ui

--- script.js
+++ script.js
@@ -52,10 +52,12 @@ function displayNotes() {
     const title = document.createElement("div");
     
     // styling
-    content.classList.add("bg-glass")
+    content.classList.add("bg-glass", "rounded-md", "mt-2")
     container.classList.add("w-72", "p-5", "bg-glass", "m-4", "rounded-r-md","rounded-l-sm", "notess", "border-l-2", "border-pink-600")
     title.classList.add("text-xl");
-    tools.classList.add("flex", "justify-between")
+    tools.classList.add("flex", "justify-between");
+    edit.classList.add('flex', 'items-center', "bg-pink-600", "px-2","py-1.5", "rounded-md", "float-right");
+    date.classList.add("text-sm", "text-gray-200")
     // text
     
     deleteButton.innerHTML = `
@@ -66,20 +68,23 @@ function displayNotes() {
       <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
       </svg>
     </button>`;
-    edit.innerHTML = "Edit";
-    content.innerHTML = `<textarea type="text" class="bg-transparent text-white border-none outline-none resize-none box-border w-full h-44 p-0 overflow-y-auto " readonly >${element.text}
+    edit.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"    fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+      <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+      <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+    </svg>&nbsp;Edit`;
+    content.innerHTML = `<textarea type="text" class="bg-transparent text-white border-none outline-none resize-none box-border w-full h-44 p-3 overflow-y-auto  " readonly >${element.text}
     </textarea>`;
     date.innerHTML = `${element.createAt}`;
-    title.innerHTML = `<h2 class="text-2xl">${element.title}</h2>`;
+    title.innerHTML = `<h2 class="text-2xl font-medium">${element.title}</h2>`;
 
     // display => container
     tools.appendChild(fakeElement);
     tools.appendChild(deleteButton);
     container.appendChild(tools)
     container.appendChild(title);
+    container.appendChild(date)
     container.appendChild(content)
     container.appendChild(edit);
-    container.appendChild(date)
 
     result.appendChild(container)
 
