62f7c442c6f98a42438bf978527b7551cfa252b3
elvirafirmansyah <freddyfirmansyah25@gmail.com>
2022-11-15 21:25:13 +0700

    feat: add button ui

--- script.js
+++ script.js
@@ -44,34 +44,48 @@ function displayNotes() {
     // create Element
     const container = document.createElement("div");
     const content = document.createElement("div");
+    const tools = document.createElement("div")
     const edit = document.createElement("button");
     const deleteButton = document.createElement('button');
+    const fakeElement = document.createElement("div");
     const date = document.createElement("h3"); 
     const title = document.createElement("div");
-
+    
     // styling
     content.classList.add("bg-glass")
-    container.classList.add("w-72", "p-5", "bg-glass", "m-4", "rounded-md", "notess")
-    edit.classList.add("float-right", "edit")
+    container.classList.add("w-72", "p-5", "bg-glass", "m-4", "rounded-r-md","rounded-l-sm", "notess", "border-l-2", "border-pink-600")
     title.classList.add("text-xl");
-
+    tools.classList.add("flex", "justify-between")
     // text
-    edit.innerHTML = "edit";
-    deleteButton.innerHTML = "delete";
-    content.innerHTML = `<textarea type="text" class="bg-transparent text-white border-none outline-none resize-none box-border w-full h-44 p-0 overflow-y-auto " readonly >${element.text}</textarea>`;
+    
+    deleteButton.innerHTML = `
+    <button type="button"
+    class="p-2.5 text-sm font-medium text-white rounded-md border border-gray-800  focus:ring-1 focus:outline-none  hover:bg-glass focus:ring-gray-800 delete-btn"
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+      <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+      </svg>
+    </button>`;
+    edit.innerHTML = "Edit";
+    content.innerHTML = `<textarea type="text" class="bg-transparent text-white border-none outline-none resize-none box-border w-full h-44 p-0 overflow-y-auto " readonly >${element.text}
+    </textarea>`;
     date.innerHTML = `${element.createAt}`;
-    title.innerHTML = `<h2>${element.title}</h2>`;
+    title.innerHTML = `<h2 class="text-2xl">${element.title}</h2>`;
 
     // display => container
-    container.appendChild(edit);
-    container.appendChild(deleteButton);
-    container.appendChild(date)
+    tools.appendChild(fakeElement);
+    tools.appendChild(deleteButton);
+    container.appendChild(tools)
     container.appendChild(title);
     container.appendChild(content)
-    
+    container.appendChild(edit);
+    container.appendChild(date)
+
     result.appendChild(container)
 
-    deleteButton.addEventListener("click", (index) => {
+    const tombolHapus = tools.querySelector(".delete-btn");
+
+    tombolHapus.addEventListener("click", (index) => {
       data_result = data_result.filter(e => {
         if (e != element) {
           return index;
